563a012f6b360ef46d16d238d0dca00f14287e1c
professorjean <107433713+professorjean@users.noreply.github.com>
2023-10-23 21:35:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,10 +13,10 @@ def hello():
     try:
         visits = redis.incr("counter")
     except RedisError:
-        visits = "<i>cannot connect to Redis, counter disabled</i>"
+        visits = "<i>Não é possível se conecta ao Redis, contador desativado</i>"
 
-    html = "<h3>Hello {name}!</h3>"            "<b>Hostname:</b> {hostname}<br/>"            "<b>Visits:</b> {visits}"
-    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
+    html = "<h3>Olá {name}!</h3>"            "<b>Servidor:</b> {hostname}<br/>"            "<b>Visitas:</b> {visits}"
+    return html.format(name=os.getenv("NAME", "Mundo"), hostname=socket.gethostname(), visits=visits)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
